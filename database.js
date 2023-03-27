fb0935b76bed7a627401105eca29326c65310eec
Nathan Roberts <nkroberts01@gmail.com>
2023-03-26 20:16:10 -0600

    Deleted test files, moved files to public

---
+++ database.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require('mongodb');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const scoreCollection = client.db('simon').collection('recipe');
+
+function addRecipe(recipe) {
+  scoreCollection.insertOne(recipe);
+}
+
+function getRecipes() {
+  const query = {recipe: {$gt: 0}};
+  const options = {
+    limit: 100,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {addRecipe, getHighScores};
