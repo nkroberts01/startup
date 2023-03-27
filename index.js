78f2688327df20df81c47199586347aa52e70df6
Nathan Roberts <nkroberts01@gmail.com>
2023-03-26 20:43:58 -0600

    Added index.js with a get and post endpoint

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const DB = require('./database');
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json);
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router;
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/recipes', async (_req, res) => {
+    const recipes = await DB.getRecipes();
+    res.send(recipes);
+});
+
+apiRouter.post('/recipe', async (req, res) => {
+    DB.addRecipe(req.body);
+    const recipes = await DB.getRecipes();
+    res.send(recipes);
+})
+
+app.use((_req, res) => {
+    res.sendFile('index.html', {root: 'public'});
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
