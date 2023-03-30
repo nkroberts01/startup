b890ea8a777cd7014ebe107841d05252b0d99361
Nathan Roberts <nkroberts01@gmail.com>
2023-03-30 14:42:26 -0600

    Got basic endpoints working

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
-// const DB = require('./database.js');
+const DB = require("./database")
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+app.use(cookieParser());
+app.use(express.json());
 
-app.use(express.json);
-  
-  
+app.post('/cookie/:name/:value', (req, res, next) => {
+  res.cookie(req.params.name, req.params.value);
+  res.send({ cookie: `${req.params.name}:${req.params.value}` });
+});
+
+app.get('/cookie', (req, res, next) => {
+  res.send({ cookie: req.cookies });
+});
+
 app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
-
 apiRouter.get('/recipes', async (_req, res) => {
   const recipes = await DB.getRecipes();
   res.send(recipes);
@@ -23,10 +30,9 @@ apiRouter.post('/recipe', async (req, res) => {
   res.send(recipes);
 })
 
-app.use((_req, res) => {
-  res.sendStatus('index.html', { root: 'public' });
-});
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.listen(port, () => {
+// Listening to a network port
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
