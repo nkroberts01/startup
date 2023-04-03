eb53fabe151a0081419622aad26903d4e327e318
Nathan Roberts <nkroberts01@gmail.com>
2023-04-02 20:09:10 -0600

    Added user endpoints to index.js

--- index.js
+++ index.js
@@ -25,6 +25,60 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUserByUsername(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.username, req.body.password);
+
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user.id,
+    });
+  }
+});
+
+apiRouter.post('auth/login', async (req, res) => {
+  const user = await DB.getUserByUsername(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user_id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Invalid Credentials' });
+});
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+apiRouter.get('/user/:username', async (req, res) => {
+  const user = await DB.getUserByUsername(req.params.username);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ username: user.username, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
 apiRouter.get('/recipes', async (_req, res) => {
   const recipes = await DB.getRecipes();
   res.send(recipes);
@@ -35,6 +89,24 @@ apiRouter.post('/recipe', async (req, res) => {
   const recipes = await DB.getRecipes();
   res.send(recipes);
 })
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
