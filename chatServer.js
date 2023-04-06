bf44ea966327f201a5de7e1fc58352ec4abacd5f
Nathan Roberts <nkroberts01@gmail.com>
2023-04-05 20:02:38 -0600

    Changed chat port

--- chatServer.js
+++ chatServer.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve up our webSocket client HTML
 app.use(express.static('./public'));
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 server = app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
