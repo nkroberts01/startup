e747d1e3591cf64567abdd7289d8cda3caf6215c
Nathan Roberts <nkroberts01@gmail.com>
2023-04-14 14:12:32 -0600

    Package update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Startup</div>
+    <body class="text-light">
+    <header>
+        <h1>Recipes 260</h1>
+        <div class="user">
+          Welcome:
+          <span class="username-top"></span>
+        </div>
+        <menu class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="search.html">Search</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">Login</a>
+            </li>
+        </menu>
+      </header>
+
+    <main class="container-fluid text-center">
+        Recipes 260
+    </main>
+
+  </body>
   );
 }
 
