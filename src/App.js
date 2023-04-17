3bef89a786109e9ed38ca9ca588da3d087ab7afa
Nathan Roberts <nkroberts01@gmail.com>
2023-04-17 17:45:08 -0600

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login'
 import { Chat } from './chat/chat'
 import { Home } from './home/home'
@@ -8,30 +9,42 @@ import { Search } from './search/search';
 
 function App() {
   return (
-    <div class="top text-light">
-    <header>
-        <h1>Recipes 260</h1>
-        <div class="user">
-          Welcome:
-          <span class="username-top"></span>
-        </div>
-        <menu class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="search.html">Search</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="home.html">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">Login</a>
-            </li>
-        </menu>
-      </header>
+    <BrowserRouter>
+      <div class="top text-light">
+      <header>
+          <h1>Recipes 260</h1>
+          <div class="user">
+            Welcome:
+            <span class="username-top"></span>
+          </div>
+          <menu class="navbar-nav">
+              <li class="nav-item">
+                  <NavLink class="nav-link" to="search">Search</NavLink>
+              </li>
+              <li class="nav-item">
+                  <NavLink class="nav-link" to="home">Home</NavLink>
+              </li>
+              <li class="nav-item">
+                  <NavLink class="nav-link" to="">Login</NavLink>
+              </li>
+          </menu>
+        </header>
 
-    <Login />
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
-  </div>
+    </div>
+  </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Address unknown</main>
 }
 
 export default App;
